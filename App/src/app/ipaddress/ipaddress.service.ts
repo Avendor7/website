de4e3a6a4762667a334fb27e985338c0207d1b15
step1041 <steve.wiggins7@gmail.com>
2016-10-15 00:03:11 -0400

    add rxjs from angular2 google tutorials

--- App/src/app/ipaddress/ipaddress.service.ts
+++ App/src/app/ipaddress/ipaddress.service.ts
@@ -1,6 +1,8 @@
 import { Injectable }    from '@angular/core';
 import { Headers, Http } from '@angular/http';
 
+import {Observable} from {'rxjs/Observable'};
+
 import 'rxjs/add/operator/toPromise';
 
 import { IpAddressComponent } from './ipaddress.component';
@@ -8,16 +10,33 @@ import { IpAddressComponent } from './ipaddress.component';
 
 @Injectable()
 export class IpAddressService {
+   
     private serverUrl = 'http://binbash.xyz/ipaddress'
+   
     constructor(private http: Http) { }
 
 
-    getAddresses(): Promise<IpAddressComponent[]>{
+    getAddresses(): Observable<IpAddressComponent[]>{
 
+        // return this.http.get(this.serverUrl)
+        //     .toPromise()
+        //     .then(response => response.json().data as IpAddressComponent[]);
         return this.http.get(this.serverUrl)
-            .toPromise()
-            .then(response => response.json().data as IpAddressComponent[]);
-
+            .map(this.extractData)
+            .catch(this.handleError);
     }
 
+    private extractData(res: Response) {
+        let body = res.json();
+        return body.data || {};
+    }
+    
+    private handleError (error: any) {
+    // In a real world app, we might use a remote logging infrastructure
+    // We'd also dig deeper into the error to get a better message
+    let errMsg = (error.message) ? error.message :
+      error.status ? `${error.status} - ${error.statusText}` : 'Server error';
+    console.error(errMsg); // log to console instead
+    return Observable.throw(errMsg);
+  }
 }
