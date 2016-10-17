b86d13a960839bd9c00f10e3d4d48b78f398540b
Stephen <steve.wiggins7@gmail.com>
2016-10-16 21:31:42 -0400

    data access should be working now

--- App/src/app/ipaddress/ipaddress.component.ts
+++ App/src/app/ipaddress/ipaddress.component.ts
@@ -1,14 +1,21 @@
-import { Component } from '@angular/core';
-import { IpAddressService } from './ipaddress.service'
+import { Component, OnInit } from '@angular/core';
+import { IpAddressService } from './ipaddress.service';
 
 @Component({
     selector: 'my-app',
     templateUrl: '/app/ipaddress/ipaddress.html',
     providers: [IpAddressService]
 })
-export class IpAddressComponent {
+export class IpAddressComponent implements OnInit {
+    
+    ipAddresses: any;
 
     constructor(private ipAddressService: IpAddressService) {
 
     }
+    public ngOnInit(){
+
+        this.ipAddressService.getAddresses().subscribe(data => this.ipAddresses);
+    }
+    
 }
